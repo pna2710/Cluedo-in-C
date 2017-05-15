b74d242c2b827e073191d72871a57f62c7531e63
Unknown <goncalo.almeida88@gmail.com>
2017-05-15 15:30:13 +0100

    menu_para_criancas

--- Cluedo_Em_C/main.c
+++ Cluedo_Em_C/main.c
@@ -3,10 +3,74 @@
 #include <stdlib.h>
 #include "Cluedo.h"
 
+void ShowMenu();
 
-int main(void) {
+int main(void)
+{
 	srand((int)time(NULL));
-	
+
+	int MenuBit;
+	do
+	{
+		ShowMenu();
+		MenuBit = 0;
+		while (MenuBit < 1 || MenuBit>4)
+		{
+			printf("\nChoose an option between 1 and 10");
+			scanf("%d", &MenuBit);
+		}
+
+		switch (MenuBit)
+		{
+		case 1:
+
+			break;
+
+		case 2:
+
+			break;
+
+		case 3:
+
+			break;
+
+		case 4:
+
+			break;
+
+		case 5:
+
+			break;
+
+		case 6:
+
+			break;
+
+
+		default:
+			break;
+		}
+	}
+
+	while (MenuBit != 6);
+	printf("\nThank you and goodbye!\n\n");
 
 	return 0;
+}
+
+void ShowMenu()
+{
+	printf("______________________________\n\n");
+	printf("\t\tCLUEDO\n\n");
+	printf("______________________________\n");
+	printf("\nChoose one of the following options:\n\n");
+	printf("1. New Game\n");
+	printf("2. Continue Game\n");
+	printf("3. Load Game\n");
+	printf("4. Options\n");
+	printf("5. Credits\n");
+	printf("6. Exit Game\n");
+	printf("______________________________\n\n");
+	printf("\t\tEnter your choice:\n\n");
+	printf("______________________________\n");
 }
